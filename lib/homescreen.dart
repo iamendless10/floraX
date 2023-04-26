d9dd0acb4276dfa376d6e76e273384b9c30b9489
PraaneshSelvaraj <praaneshselvaraj2003@gmail.com>
2023-04-26 12:18:24 +0530

    fixed error

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -233,7 +233,7 @@ class _homescreenState extends State<homescreen> {
                         onTap: (){
                           Navigator.push(
                             context,
-                            MaterialPageRoute(builder: (context) => beforetest(plant: 'hibiscus',)),
+                            MaterialPageRoute(builder: (context) => beforetest(plant: 'rice',)),
                           );
                         },
                         child: Container(
